a90ed66449cc82e56ddebcc7f34340f4d82e4b21
GabrielMontAraujo <graubx25@gmail.com>
2025-01-28 19:22:08 -0300

    Add Metrics

--- app.py
+++ app.py
@@ -1,9 +1,12 @@
 from flask import Flask
+from prometheus_flask_exporter import PrometheusMetrics
+
 app = Flask(__name__)
+metrics = PrometheusMetrics(app)  # Expor métricas automaticamente
 
 @app.route('/')
 def hello():
-    return "Olá, mundo! Este é meu pipeline CI/CD."
+    return "Olá, mundo! Este é meu pipeline CI/CD com observabilidade."
 
 if __name__ == '__main__':
     app.run(host='0.0.0.0', port=5000)
